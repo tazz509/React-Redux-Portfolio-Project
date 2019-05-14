15238bd5871d7ce3e9995d4a264ea777ca40b04b
neoDJS <kleerodofort@yahoo.fr>
2019-05-14 13:50:31 -0400

    created different controllers

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,13 @@
+class TweetsController < ApplicationController
+
+    def index
+      render json: Tweet.stream_for(current_user.id)
+    end
+  
+    def create
+      tweet = Tweet.create(body: params[:body], user_id: current_user.id)
+      render json: tweet
+    end
+  
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  resources :followers do
+    collection do
+      get 'random'
+    end
+  end
+  resources :tweets
+  get 'profile' => 'profile#index'
+  patch 'profile' => 'profile#update'
+
+  # You can have the root of your site routed with "root"
+  root 'home#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
